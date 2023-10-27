b9aa79e51690946ccae6c2c87609b8ae9ad98f4b
Hassan-elmeshwady <hassan.elmeshwady@gmail.com>
2023-10-27 10:43:38 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const imageContainer = document.querySelector(".image-container");
+    const prevButton = document.querySelector(".prev-button");
+    const nextButton = document.querySelector(".next-button");
+
+    let currentIndex = 0;
+
+    // Array of image sources
+    const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
+
+    function showImage(index) {
+        imageContainer.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    prevButton.addEventListener("click", function() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            showImage(currentIndex);
+        }
+    });
+
+    nextButton.addEventListener("click", function() {
+        if (currentIndex < images.length - 1) {
+            currentIndex++;
+            showImage(currentIndex);
+        }
+    });
+
+    showImage(currentIndex);
+});
